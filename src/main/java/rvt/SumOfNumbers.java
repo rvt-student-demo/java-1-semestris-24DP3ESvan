6c202f21877fe077715e263f8de239a4c5d919b9
24DP3ESvan <A240393ES@rvt.lv>
2025-10-08 13:57:05 +0300

    Uzdevumi 2

--- src/main/java/rvt/SumOfNumbers.java
+++ src/main/java/rvt/SumOfNumbers.java
@@ -4,24 +4,22 @@ import java.util.Scanner;
 
 public class SumOfNumbers {
 
-    public static void main(String[] main) {
+    public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int number_count = 0;
         int number_start = 0;
-        while (true) { 
+        while (true) {
             System.out.println("Insert number: ");
-            int number = Integer.valueOf(reader.nextLine()); 
-            if (number != 0){
+            int number = Integer.valueOf(reader.nextLine());
+            if (number != 0) {
                 number_count = number_count + 1;
                 number_start = number_start + number;
-            } else if (number != 0) { 
-                continue; 
-            } else {
+            } else if (number == 0) {
                 break;
             }
-        
+
         }
-        System.out.println("Count of numbers: " + number_count); 
-        System.out.println("Total Sum: " + number_start); 
+        System.out.println("Count of numbers: " + number_count);
+        System.out.println("Total Sum: " + number_start);
     }
 }
