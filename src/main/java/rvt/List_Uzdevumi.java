69dc4df2ff8b3604533e31cf39930a3be4c8aa55
24DP3ESvan <A240393ES@rvt.lv>
2025-10-30 13:25:55 +0200

    Uzdevumi 3

--- src/main/java/rvt/List_Uzdevumi.java
+++ src/main/java/rvt/List_Uzdevumi.java
@@ -6,10 +6,7 @@ import java.util.Scanner;
 public class List_Uzdevumi {
 
     public static void main(String[] args) {
-        OnlyTheseNumbers();
-        ListSize();
         OnTheList();
-        RemoveLast();
     }
     //======================================
     public static void OnlyTheseNumbers() {
@@ -34,38 +31,39 @@ public class List_Uzdevumi {
     //======================================
     public static void ListSize() {
         Scanner reader = new Scanner(System.in);
-        ArrayList<Integer> Uzd2 = new ArrayList<>();
-        int Input = 0;
+        ArrayList<String> Uzd2 = new ArrayList<>();
         while (true) {
-            if (Input == -1) {
+            String Input = reader.nextLine();
+            if (Input.isEmpty()) {
                 break;
             }
-            System.out.println("Input number");
-            Input = Integer.valueOf(reader.nextLine());
             Uzd2.add(Input);
         }
-        System.out.println("");
         for (int i = 0; i < Uzd2.size(); i++) {
-            int Out2 = Uzd2.get(i);
+            String Out2 = Uzd2.get(i);
             System.out.println(Out2);
         }
+        System.out.println("");
         System.out.println("In total: " + Uzd2.size());
         System.out.println("");
     }
     //======================================
     public static void OnTheList() {
         Scanner reader = new Scanner(System.in);
-        ArrayList<Integer> Uzd3 = new ArrayList<>();
-        int Search = 0;
-        Uzd3.add(1);
-        Uzd3.add(2);
-        Uzd3.add(3);
+        ArrayList<String> Uzd3 = new ArrayList<>();
+        while (true) {
+            String Input = reader.nextLine();
+            if (Input.isEmpty()) {
+                break;
+            }
+            Uzd3.add(Input);
+        }
         for (int i = 0; i < Uzd3.size(); i++) {
-            int Out3 = Uzd3.get(i);
+            String Out3 = Uzd3.get(i);
             System.out.println(Out3);
         }
-        System.out.println("Search for number?");
-        Search = Integer.valueOf(reader.nextLine());
+        System.out.println("Search for?");
+        String Search = reader.nextLine();
         if (Uzd3.contains(Search)) {
             System.out.println(Search + " is found");
         } else {
