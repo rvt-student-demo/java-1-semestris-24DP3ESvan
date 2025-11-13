0c681db8df773354e3f4267ab7c4905b41bb0bd4
24DP3ESvan <A240393ES@rvt.lv>
2025-11-13 13:39:30 +0200

    Uzdevumi 4

---
+++ src/main/java/rvt/Statistics.java
@@ -0,0 +1,39 @@
+package rvt;
+
+public class Statistics {
+    private int count;
+    private int sum;
+    private double average;
+
+    public Statistics() {
+        this.count = 0;
+    }
+
+    public void AddNumber(int number) {
+        this.sum += number;
+        this.count++;
+        this.average = sum / count;
+    }
+
+    public int GetCount() {
+        return this.count;
+    }
+
+    public int sum() {
+        return this.sum;
+    }
+
+    public double average() {
+        return this.average;
+    }
+    public static void main(String[] args) {
+        Statistics statistics = new Statistics();
+        statistics.AddNumber(4);
+        statistics.AddNumber(5);
+        statistics.AddNumber(1);
+        statistics.AddNumber(2);
+        System.out.println("Count: " + statistics.GetCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
+    }
+}
