f854ad22880f8d61f461f673ef13bc5010a165fc
24DP3ESvan <A240393ES@rvt.lv>
2025-11-27 11:03:27 +0200

    Part 1-5 un Part 1-6

---
+++ src/main/java/rvt/SecondsInADay.java
@@ -0,0 +1,13 @@
+package rvt;
+import java.util.Scanner;
+
+public class SecondsInADay {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many days would you like convert into seconds: ");
+        int number = Integer.valueOf(scanner.nextLine());
+        int El_Bobreto_Gondolieto_La_Bobroni_Gondoloni = number * 60 * 60 * 24;
+        System.out.println("You converted " + number + " days into " + El_Bobreto_Gondolieto_La_Bobroni_Gondoloni + " seconds");
+    }
+}
