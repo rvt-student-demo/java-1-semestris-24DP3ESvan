dc0315c613cad20935d0b682067a7db4807df543
24DP3ESvan <A240393ES@rvt.lv>
2025-12-10 15:32:27 +0200

    Chapter_44

--- src/main/java/rvt/Chapter_44.java
+++ src/main/java/rvt/Chapter_44.java
@@ -11,30 +11,30 @@ public class Chapter_44 {
     public static void Exercise1() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name: ");
-        String fullName = scanner.nextLine().trim(); 
-        int spaceIndex = fullName.indexOf(" ");
-        if (spaceIndex <= 0) {
-            System.out.println(fullName);
+        String FullName = scanner.nextLine().trim(); 
+        int Index = FullName.indexOf(" ");
+        if (Index <= 0) {
+            System.out.println(FullName);
         } else {
-            String firstName = fullName.substring(0, spaceIndex);
-            String lastName = fullName.substring(spaceIndex + 1);
-            lastName = lastName.toUpperCase();
-            System.out.println(firstName + " " + lastName);
+            String FirstName = FullName.substring(0, Index);
+            String LastName = FullName.substring(Index + 1);
+            LastName = LastName.toUpperCase();
+            System.out.println(FirstName + " " + LastName);
         }
     }
 
     public static void Exercise2() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string: ");
-        String fullName = scanner.nextLine().trim();
+        String FullName = scanner.nextLine().trim();
         int i = 0;
-        int n = fullName.length();
+        int n = FullName.length();
         int k = n;
         while (k > 0) {
             if (k == 0) {
                 break;
             }
-            System.out.println(fullName.charAt(i));
+            System.out.println(FullName.charAt(i));
             i += 1;
             k -= 1;
         }
@@ -77,7 +77,6 @@ public class Chapter_44 {
 
     public static void Exercise5() {
         Scanner scanner = new Scanner(System.in);
-
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
 
@@ -85,25 +84,19 @@ public class Chapter_44 {
                 System.out.println(line);
             }
         }
-
         scanner.close();
     }
 
     public static void Exercise6() {
         Scanner scanner = new Scanner(System.in);
-
         boolean inBlockComment = false;
-
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine().trim();
-
             if (inBlockComment) {
                 System.out.println(line);
-
                 if (line.startsWith("*/")) {
                     inBlockComment = false;
                 }
-
             } else {
                 if (line.startsWith("//")) {
                     System.out.println(line);
@@ -115,23 +108,17 @@ public class Chapter_44 {
                 }
             }
         }
-
         scanner.close();
     }
 
     public static void Exercise7() {
         Scanner scanner = new Scanner(System.in);
-
         while (true) {
             System.out.println("Enter your password:");
             String password = scanner.nextLine();
-
             if (password.length() >= 7) {
-
                 if (!password.equals(password.toLowerCase())) {
-
                     if (!password.equals(password.toUpperCase())) {
-
                         boolean hasDigit = false;
                         for (int i = 0; i < password.length(); i++) {
                             if (Character.isDigit(password.charAt(i))) {
@@ -139,7 +126,6 @@ public class Chapter_44 {
                                 break;
                             }
                         }
-
                         if (hasDigit) {
                             System.out.println("Acceptable password.");
                             break;
@@ -150,30 +136,23 @@ public class Chapter_44 {
 
             System.out.println("That password is not acceptable.\n");
         }
-
         scanner.close();
     }
 
     public static void Exercise8() {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
-
         int spaces = 0;
         int length = word.length();
-
         while (length > 0) {
             for (int i = 0; i < spaces; i++) {
                 System.out.print(" ");
             }
-
             System.out.println(word.substring(0, length));
-
             spaces++;
             length -= 2;
         }
-
         scanner.close();
     }
 }
