debed072c1a411a465e22ee6b46062ad1ded85aa
24DP3ESvan <A240393ES@rvt.lv>
2025-12-10 15:56:13 +0200

    Part 5_2

---
+++ src/main/java/rvt/Counter.java
@@ -0,0 +1,38 @@
+package rvt;
+
+public class Counter {
+    private int value;
+
+    public Counter(int value) {
+        this.value = value;
+    }
+
+    public void increase() {
+        this.value = this.value + 1;
+    }
+
+    public void decrease() {
+        this.value = this.value - 1;
+    }
+
+    public void increaseValue(int number) {
+        this.value = this.value + number;
+    }
+
+    public void decreaseValue(int number) {
+        this.value = this.value - number;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    public static void main(String[] args) {
+        Counter counter = new Counter(1);
+        counter.increase();
+        counter.increaseValue(3);
+        counter.decreaseValue(2);
+        counter.decrease();
+        System.out.println("Value now: " + counter.value());
+    }
+}
